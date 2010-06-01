454f7491f4e5f562338d721f980e31675a3d4144
Pete Elmore <pete@debu.gs>
2010-06-01 14:19:59 -0700

    Enhancement.

--- doc/examples/environment.rb
+++ doc/examples/environment.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env ruby
 # This example gives you a feel for the environment in which Watts::Resources
 # run.  By "environment", of course, I really just mean that the 'env' value
-# Rack gives you on requests is accessible from inside your resources.
+# Rack gives you on requests is accessible from inside your resources.  You can
+# request /, /foo, or whatever.  If you want to have a look at how query string
+# parsing works, try having a look at /query?asdf=jkl%3B .  This example just
+# uses the CGI library that comes with Ruby for parsing queries.
 
 require 'watts'
 require 'pp'
+require 'cgi'
 
 class WattsEnvironment < Watts::App
 	class EnvPrinter < Watts::Resource
@@ -15,9 +19,16 @@ class WattsEnvironment < Watts::App
 		}
 	end
 
+	class Queries < Watts::Resource
+		get {
+			CGI.parse(env['QUERY_STRING']).inspect rescue 'Couldn\'t parse.'
+		}
+	end
+
 	resource('/', EnvPrinter) {
 		resource('foo', EnvPrinter)
 		resource([:yeah], EnvPrinter)
+		resource('query', Queries)
 	}
 end
 
