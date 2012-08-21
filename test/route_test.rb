86ebec3eea76c4c9f807dd12f2137b50ab111439
Pete Elmore <pete@debu.gs>
2012-08-20 23:46:06 -0700

    So...this test fails.  Time to untorture some logic.

--- test/route_test.rb
+++ test/route_test.rb
@@ -6,10 +6,13 @@ class RouteTest < Test::Unit::TestCase
 		r1 = Class.new(Watts::Resource)
 		r2 = Class.new(Watts::Resource)
 		r3 = Class.new(Watts::Resource)
+		r4 = Class.new(Watts::Resource)
 
 		app = Class.new(Watts::App) {
 			resource('/', r1) {
-				resource(['one', :two], r2) { resource(/three/, r3) }
+				resource(['one', :two], r2) {
+					resource(/three/, r3) { resource('4', r4) }
+				}
 			}
 		}.new
 
@@ -19,6 +22,7 @@ class RouteTest < Test::Unit::TestCase
 		assert_nil app.match('/one/')
 		assert_equal [r2, ['two']], app.match('/one/two')
 		assert_equal [r3, ['two', 'three']], app.match('/one/two/three')
+		assert_equal [r4, ['two', 'three']], app.match('/one/two/three/4')
 		assert_nil app.match('/one/two/three/four')
 		assert_nil app.match('Just some random gibberish.  Bad client!')
 
@@ -28,6 +32,7 @@ class RouteTest < Test::Unit::TestCase
 		assert_equal '/one/asdf', app.path_to(r2, 'asdf')
 		assert_equal '/one/2/three', app.path_to(r3, '2', 'three')
 		assert_equal '/one/2/3three3', app.path_to(r3, '2', '3three3')
+		assert_equal '/one/2/3three/4', app.path_to(r4, '2', '3three')
 		assert_equal nil, app.path_to(r3, '2', '3')
 	end
 end
